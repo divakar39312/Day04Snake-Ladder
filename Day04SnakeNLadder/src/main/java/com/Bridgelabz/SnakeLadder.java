61267047f74a379d7e4dd125463a1fd4bba717da
Divakar <divakar39312@gmail.com>
2022-09-30 12:19:15 +0530

    [Divakar]:first commit for snake n ladder

---
+++ Day04SnakeNLadder/src/main/java/com/Bridgelabz/SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz;
+
+public class SnakeLadder {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            System.out.println("Welcome to Snake N Ladder Program");
+            int playerPosition = 0;
+            System.out.println("Position of the player Before Game start : "+playerPosition);
+        }
+
+    }
+
