17bae9afaee549e3849ee3f51c47294c90ea8843
Divakar <divakar39312@gmail.com>
2022-09-30 12:27:28 +0530

    [Divakar]:rolled dice from 1 to 6 added

--- Day04SnakeNLadder/src/main/java/com/Bridgelabz/SnakeLadder.java
+++ Day04SnakeNLadder/src/main/java/com/Bridgelabz/SnakeLadder.java
@@ -2,12 +2,14 @@ package com.Bridgelabz;
 
 public class SnakeLadder {
 
-        public static void main(String[] args) {
-            // TODO Auto-generated method stub
-            System.out.println("Welcome to Snake N Ladder Program");
-            int playerPosition = 0;
-            System.out.println("Position of the player Before Game start : "+playerPosition);
-        }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Welcome to Snake Ladder Program");
+        int playerPosition = 0;
+        System.out.println("Position of the player Before Game start : "+playerPosition);
+        int dice = (int)(Math.floor((Math.random()*6)+1));
+        System.out.println("Rolled Dice Number is "+ dice);
+    }
 
     }
 
